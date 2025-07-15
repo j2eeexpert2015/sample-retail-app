16b0346cc4500e302d45e6c9cefe1d6983ab719a
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2025-07-16 00:27:06 +0530

    Added JFR Support

--- src/main/java/com/example/retail/jfr/SpringJFRUtil.java
+++ src/main/java/com/example/retail/jfr/SpringJFRUtil.java
@@ -11,7 +11,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
